8591dec2bba9cafdcc780a0f93778e8b3a96ad64
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-03 18:48:26 +0900

    3章　復習問題

---
+++ chapter3.py
@@ -0,0 +1,17 @@
+#3-1
+print("1時間は", 60 * 60, "秒")
+
+#3-2
+seconds_per_hour = 3600
+
+#3-3
+print("1日は", seconds_per_hour * 24, "秒")
+
+#3-4
+seconds_per_day = 60 * 60 * 24
+
+#3-5
+print(seconds_per_day / seconds_per_hour)
+
+#3-6
+print(seconds_per_day // seconds_per_hour)
