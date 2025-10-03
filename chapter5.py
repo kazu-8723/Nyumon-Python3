8e8ec58e840436020b42915b3a585b56b2fd2575
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-03 19:27:16 +0900

    5章　復習問題(5-3まで)

---
+++ chapter5.py
@@ -0,0 +1,24 @@
+#5-1
+song = "That's - a moray!"
+print(song.replace("m", "M"))
+
+#5-2
+question = "Who are you?"
+answer = "I'm Keikouin Yuki."
+
+print("Q :" + question)
+print("A :" + answer)
+
+#5-3
+things = "aiueo %s"
+thing = "kakikukeko"
+
+'%s' % thing
+print(things % thing)
+
+#5-4
+
+#5-5
+#5-6
+#5-7
+#5-8
