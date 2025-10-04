31b2c334da963297456f46b8d4f350484bac552b
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-04 15:07:39 +0900

    8章　復習問題(8-4まで)

---
+++ chapter8.py
@@ -0,0 +1,33 @@
+#8-1
+e2f = dict(dog = "chien", cat = "chat", walrus = "morse")
+print(e2f)
+
+#8-2
+print(e2f["walrus"])
+
+#8-3
+f2e = {}
+for english, french in e2f.items():
+    f2e[french] = english
+print(f2e)
+
+#8-4
+print(f2e["chien"])
+
+#8-5
+
+#8-6
+
+#8-7
+
+#8-8
+
+#8-9
+
+#8-10
+
+#8-11
+
+#8-12
+
+#8-13
