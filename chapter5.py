0e1970146e4d0c20c2fa27f05cdefae05b2f44a0
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-04 12:47:23 +0900

    5章　復習問題

--- chapter5.py
+++ chapter5.py
@@ -17,8 +17,25 @@ thing = "kakikukeko"
 print(things % thing)
 
 #5-4
+letter = "Dear {salutation} {name}."
 
 #5-5
+print(letter.format(salutation = "hello", name = "pikatyu"))
+
 #5-6
+names = ["duck", "groud", "spitz"]
+for name in names:
+    cap_name = name.capitalize() #capitalize() 　先頭の1文字を大文字にする
+    print("%sy Mc%sface" % (cap_name, cap_name))
+
 #5-7
+names = ["duck", "groud", "spitz"]
+for name in names:
+    cap_name = name.capitalize() #capitalize() 　先頭の1文字を大文字にする
+    print("{}y Mc{}face".format(cap_name, cap_name))
+
 #5-8
+names = ["duck", "groud", "spitz"]
+for name in names:
+    cap_name = name.capitalize() #capitalize() 　先頭の1文字を大文字にする
+    print(f"{cap_name}y Mc{cap_name}face")
